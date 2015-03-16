1f3b7af1351870e588d135647e02eb96d9d38906
Alberto <alberto.plaza@8bit-studio.com>
2015-03-16 09:18:49 +0100

    Remove all project to create it again

--- Intermediate/Build/Win64/LylatDragoonEditor/Development/LylatDragoon/LylatDragoon.h
+++
@@ -1,5 +0,0 @@
-#ifndef __AUTO_LYLATDRAGOON_H__
-#define __AUTO_LYLATDRAGOON_H__
-//Last Write: 13/03/2015 14:41:26 +00:00
-#include "C:\Users\Alberto\Documents\Unreal Projects\LylatDragoon\Source\LylatDragoon\LylatDragoon.h"
-#endif//__AUTO_LYLATDRAGOON_H__
