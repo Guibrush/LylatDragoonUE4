a8d6542ab97e6e1b198ef56bc5465c19326de5af
Guybrush Threepwood <guibrush001@gmail.com>
2015-05-08 00:36:50 +0200

    Projectile added

---
+++ Source/LylatDragoon/LylatDragoonProjectile.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LylatDragoon.h"
+#include "LylatDragoonProjectile.h"
+
+
+// Sets default values
+ALylatDragoonProjectile::ALylatDragoonProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALylatDragoonProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALylatDragoonProjectile::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ Source/LylatDragoon/LylatDragoonProjectile.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "LylatDragoonProjectile.generated.h"
+
+UCLASS()
+class LYLATDRAGOON_API ALylatDragoonProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALylatDragoonProjectile();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
